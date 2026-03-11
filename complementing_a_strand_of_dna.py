f78578d2a64d1535845d511e7a522ac5e82c6ccb
jcapecci09 <jcapecci3@gmail.com>
2026-03-11 14:31:39 -0500

    Delete all files

--- complementing_a_strand_of_dna.py
+++
@@ -1,8 +0,0 @@
-d = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-
-with open('rosalind_revc (1).txt', 'r') as f:
-    dna = f.readline().strip()
-    complement = ''
-    for base in dna:
-        complement += d[base]
-print(complement[::-1])
