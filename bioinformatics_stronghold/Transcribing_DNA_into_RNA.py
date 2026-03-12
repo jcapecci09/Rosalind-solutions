0e623442934ffbfe09ac014718e5940761d66b8c
jcapecci09 <jcapecci3@gmail.com>
2026-03-11 20:33:22 -0500

    Added dataset and output directory along with adding command line interface

--- bioinformatics_stronghold/Transcribing_DNA_into_RNA.py
+++ bioinformatics_stronghold/Transcribing_DNA_into_RNA.py
@@ -1,10 +1,36 @@
+""" Transcribes DNA sequence to RNA (Thymine to uracil)
 
-# Opens DNA sequence and converts all thymines to uracil
-with open('rosalind_rna.txt', 'r') as file:
-    new_sequence = ''
-    sequence = file.readline()
-    for letter in sequence:
-        if letter == 'T':
-            letter = 'U'
-        new_sequence += letter
-    print(new_sequence)
+Author: Jimmy Capecci
+"""
+
+import argparse
+import sys
+
+def main():
+
+    # Set up parser for command-line interface
+    parser = argparse.ArgumentParser(description='Converts DNA to RNA')
+    parser.add_argument('-i', '--input', help='input file', required=True)
+    parser.add_argument('-o', '--output', help='output file', required=True)
+
+    # define infile and outfile
+    args = parser.parse_args(sys.argv[1:])
+    infile = args.input
+    outfile = args.output
+
+    # Opens DNA sequence and converts all thymines to uracil
+    # This could be performed with string method replace, but I opted to do it myself
+    with open(infile, 'r') as file:
+        new_sequence = ''
+        sequence = file.readline()
+        for letter in sequence:
+            if letter == 'T':
+                letter = 'U'
+            new_sequence += letter
+    
+    # Write RNA sequence to outfile
+    with open(outfile, 'w') as f:
+        f.write(new_sequence)
+
+if __name__ == '__main__':
+    main()
