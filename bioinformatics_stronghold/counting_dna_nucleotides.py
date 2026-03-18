d4e54062bdd09ac88fdab5e0863d364ac732a9e7
jcapecci09 <jcapecci3@gmail.com>
2026-03-18 12:02:13 -0500

    Update counting nucleotide script and added dataset

--- bioinformatics_stronghold/counting_dna_nucleotides.py
+++ bioinformatics_stronghold/counting_dna_nucleotides.py
@@ -1,14 +1,31 @@
 
-def counter(letter: str, input: str):
+import argparse
+import sys
 
+def counter(letter: str, input: str):
     count = 0 
-    print(input)
     for line in input:
-        print(line)
         if letter == line:
             count += 1
-    return count
+    return str(count)
+
+def main():
+
+    # set up parser
+    parser = argparse.ArgumentParser(description='Counts the respective number of times a nucleotides appears in a dna string')
+    parser.add_argument('-i', '--input', help='input file', required=True)
+    parser.add_argument('-o', '--output', help='output file', required=True)
+
+    # Define infile and outfile
+    args = parser.parse_args(sys.argv[1:])
+    infile = args.input
+    outfile = args.output
             
-with open('rosalind_dna (3).txt', 'r') as file:
-    data = file.readline()
-    print(f'{counter('A', data)} {counter('C', data)} {counter('G', data)} {counter('T', data)}')
+    with open(infile, 'r') as file:
+        data = file.readline()
+    
+    with open(outfile, 'w') as f:
+        f.write(f'{counter('A', data)} {counter('C', data)} {counter('G', data)} {counter('T', data)}\n')
+
+if __name__ == '__main__':
+    main()
